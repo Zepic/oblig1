383a57b8b6c6a9a40560228eabaf5aa5cfde7366
Zepic <horto@hotmail.no>
2020-09-09 11:26:29 +0200

    Initial commit

---
+++ FamilyApp.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oblig1
+{
+    public class FamilyApp
+    {
+        public List<Person> _people;
+        public FamilyApp(params Person[] people)
+        {
+            _people = new List<Person>(people);
+        }
+
+        public string FindChild(int id)
+        {
+            var child = new StringBuilder();
+            
+            foreach (var person in _people)
+            {
+                if (person.Mother != null && person.Mother.Id == id || person.Father != null && person.Father.Id == id)
+                {
+                    child.Append(person.GetChild() + "\n");
+                }
+            }
+            string hasChild = child.Length > 0 ? "\n  Barn:\n" : ""; //forkortet if settning
+            return hasChild + child;
+        }
+        
+        static string helpMessage = "hjelp => viser en hjelpetekst som forklarer alle kommandoene\r\nliste => lister alle personer med id, fornavn, fødselsår, dødsår og navn og id på mor og far om det finnes registrert. \r\nvis <id> => viser en bestemt person med mor, far og barn (og id for disse, slik at man lett kan vise en av dem)";
+        public string WelcomeMessage { get; } = helpMessage;
+        public string CommandPrompt { get; } = ":";
+
+
+        public string HandleCommand(string command)
+        {
+            if (command == "hjelp")
+            {
+                return helpMessage;
+            }
+
+            if (command == "liste")
+            {
+                string x = null;
+                foreach (var person in _people)
+                {
+                    x += person.GetDescription() + "\n";
+                }
+                return x;
+            }
+
+            if (command == "vis 1")
+            {
+                return _people[0].GetDescription() + FindChild(1);
+            }
+            if (command == "vis 2")
+            {
+                return _people[1].GetDescription() + FindChild(2);
+            }
+            if (command == "vis 3")
+            {
+                return _people[2].GetDescription() + FindChild(3);
+            }
+            if (command == "vis 4")
+            {
+                return _people[3].GetDescription() + FindChild(4);
+            }
+            if (command == "vis 5")
+            {
+                return _people[4].GetDescription() + FindChild(5);
+            }
+            if (command == "vis 6")
+            {
+                return _people[5].GetDescription() + FindChild(6);
+            }
+            if (command == "vis 7")
+            {
+                return _people[6].GetDescription() + FindChild(7);
+            }
+            if (command == "vis 8")
+            {
+                return _people[7].GetDescription() + FindChild(8);
+            }
+
+            return "Invalid Command";
+        }
+    }
+    
+}
---
+++ Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace oblig1
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int BirthYear;
+        public int DeathYear;
+        public Person Father;
+        public Person Mother;
+
+
+        public string GetDescription()
+        {
+            string returnInfo = "";
+
+            if (FirstName != null) 
+                returnInfo += FirstName + " ";
+            
+            if (LastName != null) 
+                returnInfo += LastName + " ";
+
+            if (Id != 0) 
+                returnInfo += "(Id=" + Id + ")";
+
+            if (BirthYear != 0) 
+                returnInfo += " Født: " + BirthYear;
+
+            if (DeathYear != 0) 
+                returnInfo += " Død: " + DeathYear;
+
+
+            if (Father != null)
+            {
+                returnInfo += " Far:";
+                if (Father.FirstName != null)
+                    returnInfo += " " + Father.FirstName;
+
+                returnInfo += " (Id=" + Father.Id + ")";
+                
+
+            }
+
+            if (Mother != null)
+            {
+                returnInfo += " Mor:";
+                if (Mother.FirstName != null)
+                    returnInfo += " " + Mother.FirstName;
+
+                returnInfo += " (Id=" + Mother.Id + ")";
+            }
+            return returnInfo;
+        }
+
+        public string GetChild()
+        {
+            List<string> list = new List<string>();
+            if (FirstName != null)
+            {
+                list.Add(FirstName);
+            }
+
+            if (Id != 0)
+            {
+                list.Add($"(Id={Id})");
+            }
+            if (BirthYear != 0) list.Add($"Født: {BirthYear}");
+            return "    " + string.Join(" ", list);
+        }
+    }
+}
